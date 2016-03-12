b9d2d67892b94ce39e5cc446fffb376ff1c98685
UW Portal SDK <aneville@uwaterloo.ca>
2016-03-12 10:10:29 -0500

    Created new file: javascript.js

---
+++ javascript.js
@@ -0,0 +1,63 @@
+angular.module('portalApp')
+
+// Widget controller - runs every time widget is shown
+.controller('watMealCtrl', ['$scope', '$http', '$q', 'watMealFactory', function ($scope, $http, $q, watMealFactory) {
+
+    // Widget Configuration
+    $scope.portalHelpers.config = {
+        // make 'widgetMenu.html' the template for the top right menu
+        "widgetMenu": "widgetMenu.html"
+    };
+
+    // Import variables and functions from service
+    $scope.data = watMealFactory.data;
+
+    // initialize the service
+    watMealFactory.init($scope);
+
+	// Show main view in the first column
+	$scope.portalHelpers.showView('main.html', 1);
+	
+}])
+// Factory maintains the state of the widget
+.factory('watMealFactory', ['$http', '$rootScope', '$filter', '$q', function ($http, $rootScope, $filter, $q) {
+		
+	var initialized = {value: false};
+
+	// Your variable declarations
+	var data = {value: null};
+
+	var init = function ($scope) {
+		if (initialized.value)
+			return;
+		
+		initialized.value = true;
+
+		// Place your init code here:
+		data.value={message:"Welcome to Portal SDK!"};
+	}
+
+
+	// Expose init(), and variables
+	return {
+		init: init,
+		data: data
+	};
+
+}])
+// Custom directive example
+.directive('watMealDirectiveName', ['$http', function ($http) {
+	return {
+		link: function (scope, el, attrs) {
+
+		}
+	};
+}])
+// Custom filter example
+.filter('watMealFilterName', function () {
+	return function (input, arg1, arg2) {
+		// Filter your output here by iterating over input elements
+		var output = input;
+		return output;
+	}
+});
